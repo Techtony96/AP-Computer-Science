624aedbb6899ae8b3a01be7ef92aa3f094edb4c8
Tony Pappas <techtony96@gmail.com>
2014-09-16 09:39:30 -0400

    Donut Island

---
+++ src/Utilities/Circle.java
@@ -0,0 +1,27 @@
+package Utilities;
+
+public class Circle {
+	
+	private double r;	//Radius
+	
+	public Circle(double r){
+		this.r = r;
+	}
+	
+	public double getRadius(){
+		return r;
+	}
+	
+	public double getArea(){
+		return Math.PI*r*r;
+	}
+	
+	public double getCircumference(){
+		return Math.PI*2*r;
+	}
+	
+	public double getDiameter(){
+		return r*2;
+	}
+
+}
