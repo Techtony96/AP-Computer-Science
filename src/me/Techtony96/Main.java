1f9fe025888ac25c2ba85b2f797c8b1b717729c2
Tony Pappas <techtony96@gmail.com>
2014-09-29 09:21:52 -0400

    StraightLine

--- src/me/Techtony96/Main.java
+++ src/me/Techtony96/Main.java
@@ -18,11 +18,17 @@ public class Main {
 	
 
 	public static void main(String[] args) {
+
+
+
 		Scanner in = new Scanner(System.in);
 		
 		
+		BlockLetters.TONY_PAPPAS.outputBlockName();
+
 		
-		BlockLetters.TONY_PAPPAS.outputBlockName();		
+	
+
 		
 	}
 	
