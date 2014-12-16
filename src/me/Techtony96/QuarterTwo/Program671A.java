3703947138d860bea76f1bfecb6a87205178c30e
Tony Pappas <techtony96@gmail.com>
2014-12-16 09:53:18 -0500

    Program 671A

--- src/me/Techtony96/QuarterTwo/Program671A.java
+++ src/me/Techtony96/QuarterTwo/Program671A.java
@@ -1,91 +1,94 @@
 package me.Techtony96.QuarterTwo;
 
-import java.text.DecimalFormat;
 import java.util.Scanner;
-
-import me.Techtony96.Utilities.BlockLetters;
 
 public class Program671A {
 
-	/* Program Name:
-	 * Version: 1.0
-	 * 
-	 * Author: Tony Pappas
-	 * Date: 
-	 * Platform: Java 1.6
-	 * 
-	 * Description: 
-	 */
+	/* Program Name: Converting Times
+	* Version: 1.0
+	*
+	* Author: Tony Pappas
+	* Date: 12/16/14
+	* Platform: Java 1.6
+	*
+	* Description: Convert Times
+	*/
 
 	static Scanner in = new Scanner(System.in);
 
-	public static void hoursToMinutes()
-	
-	
-	{
-		Scanner input = new Scanner (System.in);
+	//Convert Time
+	public static void hoursToMinutes() {
 		System.out.print("Enter hour(s): ");
-		int hours = input.nextInt();
+		int hours = in.nextInt();
 		System.out.println(hours + " * 60 = " + (hours * 60) + " minutes.");
 		System.out.println("");
 	}
 
-	public static void daysToHours()
-	{
-		Scanner input = new Scanner (System.in);
+	//Convert Time
+	public static void daysToHours() {
 		System.out.print("Enter day(s): ");
-		int days = input.nextInt();
+		int days = in.nextInt();
 		System.out.println(days + " * 24 = " + (days * 24) + " hours.");
 		System.out.println("");
 	}
-	public static void minutesToHours()
-	{
-		Scanner input = new Scanner (System.in);
+
+	//Convert Time
+	public static void minutesToHours() {
 		System.out.print("Enter minute(s): ");
-		int minutes = input.nextInt();
-		System.out.println(minutes + " / 60 = " + ((double)minutes / 60) + " hours.");
+		int minutes = in.nextInt();
+		System.out.println(minutes + " / 60 = " + ((double) minutes / 60) + " hours.");
 		System.out.println("");
 	}
-	public static void hoursToDays()
-	{
-		Scanner input = new Scanner (System.in);
+
+	//Convert Time
+	public static void hoursToDays() {
 		System.out.print("Enter hour(s): ");
-		int hours = input.nextInt();
-		System.out.println(hours + " / 24 = " + ((double)hours / 24) + " days.");
+		int hours = in.nextInt();
+		System.out.println(hours + " / 24 = " + ((double) hours / 24) + " days.");
 		System.out.println("");
 	}
-	public static void elapsedTime()
-	{
-		Scanner input = new Scanner (System.in);
+
+	//find time inbetween
+	public static void elapsedTime() {
 		System.out.print("Enter the beginning hour: ");
-		int startingHour = input.nextInt();
+		int startingHour = in.nextInt();
 		System.out.print("Enter the beginning minute(s): ");
-		int startingMinutes = input.nextInt();
+		int startingMinutes = in.nextInt();
 		System.out.print("Enter AM/PM: ");
-		String startingAmOrPm = input.nextLine();
+		String startingAmOrPm = in.nextLine();
+		in.nextLine();
+		if (startingAmOrPm.toLowerCase().contains("p"))
+			startingHour += 12;
 		System.out.print("Enter the ending hour: ");
-		int endingHour = input.nextInt();
+		int endingHour = in.nextInt();
 		System.out.print("Enter the ending minute(s): ");
-		int endingMinutes = input.nextInt();
+		int endingMinutes = in.nextInt();
 		System.out.print("Enter AM/PM: ");
-		String endingAmOrPm = input.nextLine();
+		in.nextLine();
+		String endingAmOrPm = in.nextLine();
+		if (endingAmOrPm.toLowerCase().contains("p"))
+			endingHour += 12;
+		
+		int differenceInMinutes = (endingHour*60+endingMinutes) - (startingHour*60+startingMinutes);
+		
+		int differenceHours = differenceInMinutes/60;
+		int differenceMinutes = differenceInMinutes%60;
+		
 		System.out.println("");
-		System.out.println("The elapsed time is: " + );
+		System.out.println("The elapsed time is: " + differenceHours + "hrs and " + differenceMinutes + " mins.");
 	}
-	public static void main (String args [])
-	{
-		int x = 1;
-		while (x == 1) {
-			Scanner input = new Scanner (System.in);
+
+	//main method
+	public static void main(String args[]) {
+		while (true) {
 			System.out.println("Conversion Tasks");
-			System.out.println("\t1. Hours -> Minutes");
-			System.out.println("\t2. Days -> Hours");
-			System.out.println("\t3. Minutes -> Hours");
-			System.out.println("\t4. Hours -> Days");
-			System.out.println("\t5. Elapsed time between two times");
-			System.out.println("\t6. Exit");
+			System.out.println("1. Hours -> Minutes");
+			System.out.println("2. Days -> Hours");
+			System.out.println("3. Minutes -> Hours");
+			System.out.println("4. Hours -> Days");
+			System.out.println("5. Elapsed time between two times");
 			System.out.print("Enter a number: ");
-			int menu = input.nextInt();
+			int menu = in.nextInt();
 			System.out.println("");
 			if (menu == 1)
 				hoursToMinutes();
@@ -97,23 +100,34 @@ public class Program671A {
 				hoursToDays();
 			if (menu == 5)
 				elapsedTime();
-			if (menu == 6)
-				x = 0;
 		}
 	}
 }
-
-
-
-}
 /*
 What I Learned:
-
+Calculations
 
 Difficulties I had:
-
+in.nextLine() after an in.nextInt() returns \n
 
 Sample Output:
+Conversion Tasks
+1. Hours -> Minutes
+2. Days -> Hours
+3. Minutes -> Hours
+4. Hours -> Days
+5. Elapsed time between two times
+Enter a number: 1
 
+Enter hour(s): 2
+2 * 60 = 120 minutes.
 
- */
+Conversion Tasks
+1. Hours -> Minutes
+2. Days -> Hours
+3. Minutes -> Hours
+4. Hours -> Days
+5. Elapsed time between two times
+Enter a number: 
+
+*/
