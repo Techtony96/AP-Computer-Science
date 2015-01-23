0a7479ceaee16b62ff77689c8a90562f32c82230
Tony Pappas <techtony96@gmail.com>
2015-01-23 09:34:54 -0500

    Guessing Game

---
+++ src/me/Techtony96/QuarterThree/Week20/Thursday/Player.java
@@ -0,0 +1,25 @@
+package me.Techtony96.QuarterThree.Week20.Thursday;
+
+import java.util.Scanner;
+
+public class Player {
+	int number;
+	String name;
+
+	public Player(){
+		System.out.print("Player, enter your name: ");
+		setName(new Scanner(System.in).nextLine());
+		
+	}
+	
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	public void guess(){
+		System.out.println(name + ", enter your guess: ");
+		number = new Scanner(System.in).nextInt();
+	}
+
+}
