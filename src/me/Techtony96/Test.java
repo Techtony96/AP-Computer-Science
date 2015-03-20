9fd7b49a1f477ddea69d2641b3a3f4f8815cd812
Tony Pappas <techtony96@gmail.com>
2015-03-20 09:35:23 -0400

    Remove JavaDocs from picture lab

--- src/me/Techtony96/Test.java
+++ src/me/Techtony96/Test.java
@@ -1,6 +1,20 @@
 package me.Techtony96;
 
+import java.util.Arrays;
+
 public class Test {
 
+	public static void main(String[] args) {
+		int[] A = { 1, 2, 3 };
+		int k = 0;
+		int key = 10;
+		while ((k < A.length ) && (A[k] != key))
+			k++;
+		if (A[k] == key) {
+			System.out.println("T");
+		}
+
+		System.out.println("F");
+	}
 
 }
